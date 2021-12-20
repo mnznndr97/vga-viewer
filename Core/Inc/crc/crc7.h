abd9aead4d036fe223c28a57396c4105754a6dc6
Andrea Monzani <mnznndr97@outlook.it>
2021-12-20 17:04:42 +0100

    Fixed CRC7 code for unaligned access

--- Core/Inc/crc/crc7.h
+++ Core/Inc/crc/crc7.h
@@ -1,8 +1,14 @@
 /*
- * SD specific CRC7 implementation.
- *
+ * This file contains the headers of the SD specific CRC7 implementation.
+ * Before using the CRC7 implementation, the Initialize function must be called.
+ * Each CRC calculation must start usinf the CRC7_ZERO value
+ * 
  * For more information see SD Physical Layer Simplified Specification - Section 4.5 (CRC)
  *
+ * @remarks The polynomial is fixed in the implementation using the SD specification
+ * but this can be generalized in a future using a kinda CRC handle initialized using a
+ * custom polynomial
+ * 
  *  Created on: May 18, 2021
  *      Author: Andrea Monzani [Mat 952817]
  */
@@ -10,15 +16,23 @@
 #ifndef CRC7_H_
 #define CRC7_H_
 
+/// Initial CRC value
 #define CRC7_ZERO 0
 
 #include <stdint.h>
 #include <stddef.h>
 
+/// Initialize the CRC calculation componet by calculating the CRC table for each byte
 void Crc7Initialize();
-
+/// Add the CRC for a byte to a current existing CRC value (incremental calculation)
+/// @param crc Current CRC value
+/// @param data New byte to add to the CRC
+/// @return 
 uint8_t Crc7Add(uint8_t crc, uint8_t data);
-
-uint8_t Crc7Calculate(const uint8_t *pData, size_t length);
+/// @brief Calculates the CRC of a stream of data
+/// @param pData Data pointer
+/// @param count Number of bytes to read from the pointer
+/// @return Data CRC
+uint8_t Crc7Calculate(const uint8_t *pData, size_t count);
 
 #endif /* CRC7_H_ */
--- Core/Src/crc/crc7.c
+++ Core/Src/crc/crc7.c
@@ -41,26 +41,41 @@ uint8_t Crc7Add(uint8_t crc, uint8_t data) {
 	return CRCTable[index];
 }
 
-uint8_t Crc7Calculate(const uint8_t *pData, size_t length) {
+uint8_t Crc7Calculate(const uint8_t *pData, size_t count) {
+	if (count == 0 || pData == NULL)
+		return CRC7_ZERO;
+
 	uint8_t crc = CRC7_ZERO;
+	// Until pointer is not aligned, we have to access byte per byte
+	while ((((uintptr_t) pData) & 0x3)) {
+		crc = Crc7Add(crc, *pData);
 
-	size_t byteOffset = 0;
-	const uint32_t *pU32 = (const uint32_t*) pData;
-	for (size_t i = 0; i < (length / 4); i++, byteOffset += 4) {
-		uint32_t data = pU32[i];
+		// We update pointer and number of item to read
+		++pData;
+		--count;
+	}
 
-        // NB: data are read in little endian order in our U32 so
-        // we must crc them in "reverse" order
+	// Until we can read a DWord, we access the 32bit data to gain some performance
+	while (count >= 4) {
+		uint32_t data = *((const uint32_t*) pData);
+
+		// NB: data are read in little endian order in our U32 so
+		// we must crc them in "reverse" order
 		crc = Crc7Add(crc, ((data) & 0xFF));
 		crc = Crc7Add(crc, ((data >> 8) & 0xFF));
 		crc = Crc7Add(crc, ((data >> 16) & 0xFF));
 		crc = Crc7Add(crc, ((data >> 24) & 0xFF));
+
+		pData += 4;
+		count -= 4;
 	}
 
-    // Let's also handle trailing bytes
-	for (size_t i = byteOffset; i < length; i++) {
-		uint8_t data = pData[i];
-		crc = Crc7Add(crc, data);
+	// Let's also handle trailing bytes
+	while (count > 0) {
+		crc = Crc7Add(crc, *pData);
+
+		++pData;
+		--count;
 	}
 
 	return crc;
